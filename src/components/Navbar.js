0fef5d5c8496a90bd506bc15eaa910cb3f3a0a51
lennartchua59 <ihighigt@gmail.com>
2023-12-05 22:34:25 +0800

    added sign in button for webview navbar  #3  #1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { AiOutlineClose, AiOutlineMenu, AiOutlineInfoCircle } from 'react-icons/
 import isate from "../images/isate2024-logo.png"
 import { Link } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
-
+import '../styles/navbar.css'
 const Navbar = () => {
   const [nav, setNav] = useState(false);
 
@@ -33,18 +33,26 @@ const Navbar = () => {
         </div>
       <ul className={nav ? 'fixed left-0 top-20 w-[60%] h-full bg-[#EDF1F4] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
         <h3 className='w-full font-bold text-[#000] pt-10'>{ user?.displayName }</h3>
-        <p className='font-bold text-[grey]'>{ user?.displayName ? (<button onClick={handleSignOut}>Logout</button>) : <Link to="/signin">Sign In</Link>}</p>
+        <p className='font-bold text-[grey]'>{ user?.displayName ? (<button onClick={handleSignOut}>Logout</button>) : <Link to="/signin">Log In or Sign Up</Link>}</p>
           <li className='p-4'><Link to="/" className='no-underline text-black'>Home</Link></li>
           <li className='p-4'><Link to="/map" className='no-underline text-black'>Map</Link></li>
           <li className='p-4'><Link to="/announcement" className='no-underline text-black'>Announcement</Link></li>
           <li className='p-4 '><Link to="/connect" className='no-underline text-black'>Connect</Link></li>
           <li className='p-4'><Link to="/help" className='no-underline text-black'>Help</Link></li>
       </ul>
+
+
+
+
+
       
       <div className='hidden md:flex ml-10'>
-        <Link to="/">
-          <img className='object-fit h-12 w-36' src={isate} alt="istate" />
-        </Link>
+      
+      <div className="login-container">
+      <h3 className='w-full font-bold text-[#000] pt-10'>{ user?.displayName }</h3>
+        <p className='font-bold text-[grey]'>{ user?.displayName ? (<button onClick={handleSignOut}>Logout</button>) : <Link style={{ textDecoration: 'none' }} to="/signin">Log In or Sign Up</Link>}</p>
+  </div>
+
       </div>
       <ul className='hidden md:flex mr-20 pt-3'>
         <li className='p-4' href="/"><Link to="/" className='no-underline text-black'>Home</Link></li>
