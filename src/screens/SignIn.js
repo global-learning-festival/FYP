ceb342bba9165bd5ed96a4e756589de70fc7f096
DylanFromSP <92902711+DylanFromSP@users.noreply.github.com>
2023-12-08 01:57:32 +0800

    Google Login Card Design Login #3

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -26,9 +26,16 @@ const SignIn = () => {
 
   return (
     <>
-    <h1 className='text-center'>Google OAuth Sign In</h1>
-        <div className='max-w-[240px] m-auto py-4'>
-            <GoogleButton onClick={handleGoogleSignIn} />
+        <div class="flex items-center justify-center mt-20">
+            <div class="flex flex-col items-center w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
+                <h5 class="mb-3 text-base font-semibold text-gray-900 md:text-xl">
+                Sign In With Google
+                </h5>
+                <p class="flex flex-col items-center text-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                Have access to personalised features by signing in with Google!
+                </p>
+                <GoogleButton onClick={handleGoogleSignIn} />
+            </div>
         </div>
     </>
   )
