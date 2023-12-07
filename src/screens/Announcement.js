2f4b02f6e6d7c8e78de4faecb0b0ff1844430fa2
DylanFromSP <92902711+DylanFromSP@users.noreply.github.com>
2023-12-08 01:35:50 +0800

    Dummy Content Announcements #13

--- src/screens/Announcement.js
+++ src/screens/Announcement.js
@@ -3,63 +3,118 @@ import React from 'react'
 const Announcement = () => {
   return (
     <>
-  {/* First set of 3 cards */}
-    <div className='flex justify-center mt-5'>
-      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
-          Announcement 1
-        </h5>
-        <p class="font-normal text-gray-500">
-          Here are announcement 1 contents.
-        </p>
+      {/* First set of 3 cards */}
+      <div className='flex justify-center my-3'>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Wifi Passwords
+          </h5>
+          <p class="text-xs text-gray-500">
+            Wi-Fi passwords for the conference venue will be shown here.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Emergency Contacts
+          </h5>
+          <p class="text-xs text-gray-500">
+            Important phone numbers for onsite medical assistance, security, and conference organizers will be provided in case of emergencies.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Lost and Found
+          </h5>
+          <p class="text-xs text-gray-500">
+            Instructions on how to report lost items or find lost-and-found locations will be included.
+          </p>
+        </div>
       </div>
-      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
-          Announcement 1
-        </h5>
-        <p class="font-normal text-gray-500">
-          Here are announcement 1 contents.
-        </p>
-      </div>
-      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
-          Announcement 1
-        </h5>
-        <p class="font-normal text-gray-500">
-          Here are announcement 1 contents.
-        </p>
-      </div>
-    </div>
 
-    {/* Second set of 3 cards */}
-    <div className='flex justify-center mt-5'>
-      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
-          Announcement 1
-        </h5>
-        <p class="font-normal text-gray-500">
-          Here are announcement 1 contents.
-        </p>
+      {/* Second set of 3 cards */}
+      <div className='flex justify-center my-3'>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Photography and Recording Policy
+          </h5>
+          <p class="text-xs text-gray-500">
+            Attendees will be informed about the conference's policy on photography, audio/video recording, and social media usage during sessions.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Conference Room Changes
+          </h5>
+          <p class="text-xs text-gray-500">
+            Some conference room changes have been made. Please check the updated schedule for details.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Asian Guest Speaker Talks Postponed
+          </h5>
+          <p class="text-xs text-gray-500">
+            The talks by Asian guest speakers have been postponed. New schedule to be announced soon.
+          </p>
+        </div>
       </div>
-      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
-          Announcement 1
-        </h5>
-        <p class="font-normal text-gray-500">
-          Here are announcement 1 contents.
-        </p>
-      </div>
-      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
-          Announcement 1
-        </h5>
-        <p class="font-normal text-gray-500">
-          Here are announcement 1 contents.
-        </p>
-      </div>
-    </div>
-  </>
 
+      {/* Third set of 3 cards */}
+      <div className='flex justify-center my-3'>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Blocked Walking Paths
+          </h5>
+          <p class="text-xs text-gray-500">
+            Please be aware of blocked walking paths in the conference venue due to ongoing maintenance work.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Dining and Refreshments
+          </h5>
+          <p class="text-xs text-gray-500">
+            Details about meal times, refreshment stations, dietary options, and nearby eateries will be listed.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Transportation
+          </h5>
+          <p class="text-xs text-gray-500">
+            Information on transportation options to and from the venue, including public transit, parking details, and ride-sharing services, will be available.
+          </p>
+        </div>
+      </div>
+
+      {/* Fourth set of 3 cards */}
+      <div className='flex justify-center my-3'>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Lost Badge or Ticket
+          </h5>
+          <p class="text-xs text-gray-500">
+            Instructions on what to do in case an attendee loses their conference badge or ticket will be included.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Sustainability Efforts
+          </h5>
+          <p class="text-xs text-gray-500">
+            Information on the conference's sustainability initiatives, such as recycling and reducing waste, will be highlighted.
+          </p>
+        </div>
+        <div class="flex-1 block mx-2 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+            Announcement 12
+          </h5>
+          <p class="font-normal text-gray-500">
+            Here are announcement 12 contents.
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
