28b94c57888418426de393c753d00ad3054bf78e
DylanFromSP <92902711+DylanFromSP@users.noreply.github.com>
2023-12-08 00:57:46 +0800

    Announcement Initial Design Announcements #13

--- src/screens/Announcement.js
+++ src/screens/Announcement.js
@@ -2,7 +2,64 @@ import React from 'react'
 
 const Announcement = () => {
   return (
-    <div>Announcement</div>
+    <>
+  {/* First set of 3 cards */}
+    <div className='flex justify-center mt-5'>
+      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+          Announcement 1
+        </h5>
+        <p class="font-normal text-gray-500">
+          Here are announcement 1 contents.
+        </p>
+      </div>
+      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+          Announcement 1
+        </h5>
+        <p class="font-normal text-gray-500">
+          Here are announcement 1 contents.
+        </p>
+      </div>
+      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+          Announcement 1
+        </h5>
+        <p class="font-normal text-gray-500">
+          Here are announcement 1 contents.
+        </p>
+      </div>
+    </div>
+
+    {/* Second set of 3 cards */}
+    <div className='flex justify-center mt-5'>
+      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+          Announcement 1
+        </h5>
+        <p class="font-normal text-gray-500">
+          Here are announcement 1 contents.
+        </p>
+      </div>
+      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+          Announcement 1
+        </h5>
+        <p class="font-normal text-gray-500">
+          Here are announcement 1 contents.
+        </p>
+      </div>
+      <div class="block mx-4 max-w-sm p-6 bg-white border border-gray-200 rounded-md shadow">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+          Announcement 1
+        </h5>
+        <p class="font-normal text-gray-500">
+          Here are announcement 1 contents.
+        </p>
+      </div>
+    </div>
+  </>
+
   )
 }
 
