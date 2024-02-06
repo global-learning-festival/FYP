508604d7baae3d8421575d024daa7e86125fed43
Ranyuro <asphyxiaranyuro@gmail.com>
2024-02-06 14:49:53 +0800

    Linkedin Changes Connect #16

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   AiOutlineClose,
   AiOutlineMenu,
@@ -10,13 +10,16 @@ import "../styles/navbar.css";
 import isate2024Logo from "../images/isate2024-logo.png";
 import QRCodeGenerator from "../screens/QrCodeGenerator";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Navbar = () => {
+  const localhostapi = "http://localhost:5000";
+  const serverlessapi = "https://adminilftest.onrender.com";
   const [nav, setNav] = useState(false);
   const [showImportantInfo, setShowImportantInfo] = useState(false);
   const loggedInUserID = localStorage.getItem("loggedInUserID");
   const [userData, setUserData] = useState(null);
-  const { userid } = useParams();
+  const [user2, setUser2] = useState({});
 
   const { user, logout } = UserAuth();
 
@@ -37,6 +40,23 @@ const Navbar = () => {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("UID:", loggedInUserID); // Log the uid to check if it's correct
+        const response = await axios.get(
+          `${serverlessapi}/useruid/${loggedInUserID}`
+        );
+        console.log("API Response:", response.data);
+        setUser2(response.data);
+      } catch (error) {
+        console.error("Error fetching user information:", error);
+      }
+    };
+
+    fetchData();
+  }, [loggedInUserID]);
 
   return (
     <div className="bg-[#FFF] flex justify-between items-center h-20 mt-auto px-4 text-black z-100">
@@ -61,16 +81,20 @@ const Navbar = () => {
         }
       >
         <h3 className="w-full font-bold text-[#000] mt-10 ml-12 mb-2.5">
-          {user?.displayName}
+          {(user?.displayName && user?.displayName) ||
+            (user2?.first_name && user2?.last_name
+              ? `${user2.first_name} ${user2.last_name}`
+              : null)}
         </h3>
-        {user ? (
+        {(user || user2) && (
           <button
             className="text-white bg-[#4B558A] font-medium rounded-md text-sm px-5 py-2.5 mx-5 hover:bg-[#3A426C] hover:drop-shadow-xl"
             onClick={handleSignOut}
           >
             Logout
           </button>
-        ) : (
+        )}
+        {!user && !user2 && (
           <button
             onClick={handleNav}
             className="text-white bg-[#4B558A] font-medium rounded-md text-sm px-5 py-2.5 mx-5 hover:bg-[#3A426C] hover:drop-shadow-xl"
@@ -128,16 +152,20 @@ const Navbar = () => {
 
       <div className="hidden md:flex ml-10">
         <h4 className="w-full font-bold text-[#000] mt-2 mr-3">
-          {user?.displayName}
+          {(user?.displayName && user?.displayName) ||
+            (user2?.first_name && user2?.last_name
+              ? `${user2.first_name} ${user2.last_name}`
+              : null)}
         </h4>
-        {user ? (
+        {(user || user2) && (
           <button
             className="text-white bg-[#4B558A] font-medium rounded-md text-sm px-5 py-2.5 mx-5 hover:bg-[#3A426C] hover:drop-shadow-xl"
             onClick={handleSignOut}
           >
             Logout
           </button>
-        ) : (
+        )}
+        {!user && !user2 && (
           <button className="text-white bg-[#4B558A] font-medium rounded-md text-sm px-5 py-2.5 mx-5 hover:bg-[#3A426C] hover:drop-shadow-xl">
             <Link
               to="/signin"
